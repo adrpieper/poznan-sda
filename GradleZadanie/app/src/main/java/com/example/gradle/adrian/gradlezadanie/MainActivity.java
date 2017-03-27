eb9a0b100b3c803eb4ca959a7ab0751d2061d184
Adrian <adrpieper@gmail.com>
2017-03-27 19:02:30 +0200

    Dodałem id do pola textowego

--- GradleZadanie/app/src/main/java/com/example/gradle/adrian/gradlezadanie/MainActivity.java
+++ GradleZadanie/app/src/main/java/com/example/gradle/adrian/gradlezadanie/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gradle.adrian.gradlezadanie;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textField);
     }
 }
