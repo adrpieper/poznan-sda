1c13b4313e55f6bdc782758e77b5db0d021d07e8
Adrian <adrpieper@gmail.com>
2017-03-27 19:42:25 +0200

    Dodałem wyświetlanie maksymalnej wartości

--- GradleZadanie/app/src/main/java/com/example/gradle/adrian/gradlezadanie/MainActivity.java
+++ GradleZadanie/app/src/main/java/com/example/gradle/adrian/gradlezadanie/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.ArrayMath;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -12,5 +14,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.textField);
+        int[] numbers = {1,2,32,1,2};
+        textView.setText(Integer.toString(ArrayMath.max(numbers)));
     }
 }
