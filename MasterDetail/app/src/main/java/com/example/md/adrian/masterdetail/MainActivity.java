72dabe6697f2fd9b5a2364aa7ec448bcf12ee0ec
Adrian <adrpieper@gmail.com>
2017-03-27 20:55:14 +0200

    Utworzyłem projekt master-detail (zadanie 2)

---
+++ MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.md.adrian.masterdetail;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
