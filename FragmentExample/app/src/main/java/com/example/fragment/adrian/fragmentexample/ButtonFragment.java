9e62dffc646a46cd633f6b1a601f998b3d486ab5
Adrian <adrpieper@gmail.com>
2017-03-27 20:40:58 +0200

    Dodałem wyświetlanie komunikatu po wciśnięciu przycisku

--- FragmentExample/app/src/main/java/com/example/fragment/adrian/fragmentexample/ButtonFragment.java
+++ FragmentExample/app/src/main/java/com/example/fragment/adrian/fragmentexample/ButtonFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragment.adrian.fragmentexample;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,11 +14,17 @@ import android.widget.Button;
 
 public class ButtonFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Button button = new Button(getActivity());
         button.setText("OK");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.showToast();
+            }
+        });
         return button;
     }
 }
