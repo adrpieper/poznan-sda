270166f8e39de33db81dbb22141fc1dfc43589bb
Adrian <adrpieper@gmail.com>
2017-03-28 18:59:43 +0200

    Dodałem listenery do przycisków z fragmentu master

--- MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MainActivity.java
+++ MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.md.adrian.masterdetail;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MasterFragment.java
+++ MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MasterFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Adrian on 2017-03-28.
@@ -14,6 +15,26 @@ public class MasterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.master_layout, container);
+
+        MainActivity activity = (MainActivity) getActivity();
+        View view = inflater.inflate(R.layout.master_layout, container);
+        Button basicDataButton = (Button) view.findViewById(R.id.basicDataButton);
+        Button detailsButton = (Button) view.findViewById(R.id.detailsButton);
+        basicDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        detailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
+
+
     }
 }
