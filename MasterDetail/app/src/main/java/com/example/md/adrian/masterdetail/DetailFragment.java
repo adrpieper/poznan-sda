05f544ee3a5c3cf86775bed95e5db1a9f0b7b41e
Adrian <adrpieper@gmail.com>
2017-03-28 18:36:27 +0200

    Skleiłem fragmenty i layouty w całość

--- MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/DetailFragment.java
+++ MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/DetailFragment.java
@@ -1,10 +1,19 @@
 package com.example.md.adrian.masterdetail;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Adrian on 2017-03-28.
  */
 
 public class DetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.detail_layout, container);
+    }
 }
