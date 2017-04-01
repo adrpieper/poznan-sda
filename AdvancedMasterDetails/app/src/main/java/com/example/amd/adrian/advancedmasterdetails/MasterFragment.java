ad20757602f8e541a7ef2c9afdde7b4123bb5d9d
Adrian <adrpieper@gmail.com>
2017-04-01 12:44:12 +0200

    Dodałem kilka zasobów

--- AdvancedMasterDetails/app/src/main/java/com/example/amd/adrian/advancedmasterdetails/MasterFragment.java
+++ AdvancedMasterDetails/app/src/main/java/com/example/amd/adrian/advancedmasterdetails/MasterFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 /**
@@ -16,14 +18,17 @@ public class MasterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_master, container, false);
-        Button currencyCalculatorButton = (Button) view.findViewById(R.id.currencyCalculatorButton);
+        final Button currencyCalculatorButton = (Button) view.findViewById(R.id.currencyCalculatorButton);
         Button investmentButton = (Button) view.findViewById(R.id.investmentButton);
         final MainActivity activity = (MainActivity) getActivity();
 
         currencyCalculatorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activity.showCurrencyCalculator();
+                //activity.showCurrencyCalculator();
+
+                Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.simple_animation);
+                currencyCalculatorButton.startAnimation(animation);
             }
         });
         investmentButton.setOnClickListener(new View.OnClickListener() {
