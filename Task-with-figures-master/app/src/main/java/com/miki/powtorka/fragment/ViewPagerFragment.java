d1106d8537f8d1fbc3c247c69644fd421a4f2348
Adrian <adrpieper@gmail.com>
2017-04-04 20:27:10 +0200

    Dodałem tłumaczenie

--- Task-with-figures-master/app/src/main/java/com/miki/powtorka/fragment/ViewPagerFragment.java
+++ Task-with-figures-master/app/src/main/java/com/miki/powtorka/fragment/ViewPagerFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.miki.powtorka.R;
+
 /**
  * Created by Adrian on 2017-04-04.
  */
@@ -20,7 +22,7 @@ public class ViewPagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        ViewPager viewPager = new ViewPager(getContext());
+        ViewPager viewPager = (ViewPager) inflater.inflate(R.layout.pager_titles, container, false);
         viewPager.setAdapter(new MyAdapter(getFragmentManager()));
         return viewPager;
     }
@@ -28,6 +30,7 @@ public class ViewPagerFragment extends Fragment {
     private class MyAdapter extends FragmentPagerAdapter {
 
         public static final int NUMBER_OF_PAGES = 3;
+        private final int[] names = {R.string.circle, R.string.rectangle, R.string.triangle};
 
         public MyAdapter(FragmentManager fm) {
             super(fm);
@@ -48,6 +51,11 @@ public class ViewPagerFragment extends Fragment {
         @Override
         public int getCount() {
             return NUMBER_OF_PAGES;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(names[position]);
         }
     }
 }
