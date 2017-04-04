ba62905b6b41aec8adbb9d0dc9aa9db9058a1487
Adrian <adrpieper@gmail.com>
2017-04-04 19:13:14 +0200

    Dodałem szkielet ViewPager

---
+++ Task-with-figures-master/app/src/main/java/com/miki/powtorka/fragment/ViewPagerFragment.java
@@ -0,0 +1,44 @@
+package com.miki.powtorka.fragment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Adrian on 2017-04-04.
+ */
+
+public class ViewPagerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        ViewPager viewPager = new ViewPager(getContext());
+
+        return viewPager;
+    }
+
+    private class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
+}
