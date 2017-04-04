5626f4e56ea7ed3ab93e2a729664ba3780c187b3
Adrian <adrpieper@gmail.com>
2017-04-04 19:50:28 +0200

    Dodałem FragmentFactory pattern

--- Task-with-figures-master/app/src/main/java/com/miki/powtorka/fragment/ViewPagerFragment.java
+++ Task-with-figures-master/app/src/main/java/com/miki/powtorka/fragment/ViewPagerFragment.java
@@ -19,35 +19,51 @@ public class ViewPagerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-
+        // () -> new FragmentTriangle()
         ViewPager viewPager = new ViewPager(getContext());
-        viewPager.setAdapter(new MyAdapter(getFragmentManager()));
+        viewPager.setAdapter(new MyAdapter(getFragmentManager(),
+
+                new FragmentFactory() {
+                    @Override
+                    public Fragment create() {
+                        return new FragmentTriangle();
+                    }
+                }, new FragmentFactory() {
+                    @Override
+                    public Fragment create() {
+                        return new FragmentRectangle();
+                    }
+                }, new FragmentFactory() {
+                    @Override
+                    public Fragment create() {
+                        return new FragmentCircle();
+                    }
+                })
+        );
         return viewPager;
     }
 
     private class MyAdapter extends FragmentPagerAdapter {
 
-        public static final int NUMBER_OF_PAGES = 3;
+        public final FragmentFactory[] factories;
 
-        public MyAdapter(FragmentManager fm) {
+        public MyAdapter(FragmentManager fm, FragmentFactory... factories) {
             super(fm);
+            this.factories = factories;
         }
 
         @Override
         public Fragment getItem(int position) {
-            if (position == 0) {
-                return new FragmentCircle();
-            }
-            if (position == 1) {
-                return new FragmentRectangle();
-            }
-
-            return new FragmentTriangle();
+            return factories[position].create();
         }
 
         @Override
         public int getCount() {
-            return NUMBER_OF_PAGES;
+            return factories.length;
         }
+    }
+
+    public interface FragmentFactory {
+        Fragment create();
     }
 }
