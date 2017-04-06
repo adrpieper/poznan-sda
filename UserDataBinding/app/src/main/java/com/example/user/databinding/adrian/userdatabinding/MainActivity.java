729d245a97e1da5a8b7be84332fefb22d7ba9ccf
Adrian <adrpieper@gmail.com>
2017-04-06 19:31:34 +0200

    Dodałem settery

--- UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/MainActivity.java
+++ UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setUser(new User("Jan", "Kowalski"));
+        final User user = new User("Jan", "Kowalski");
+        binding.setUser(user);
         binding.nextPersonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                binding.setUser(new User("Adam", "Dominik"));
+                user.setFirstName("Adam");
+                user.setLastName("Dominik");
             }
         });
     }
--- UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/User.java
+++ UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/User.java
@@ -1,24 +1,39 @@
 package com.example.user.databinding.adrian.userdatabinding;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by Adrian on 2017-04-06.
  */
 
-public class User {
+public class User extends BaseObservable {
 
-    private final String firstName;
-    private final String lastName;
+    private String firstName;
+    private String lastName;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    @Bindable
     public String getFirstName() {
         return this.firstName;
     }
 
+    @Bindable
     public String getLastName() {
         return this.lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
     }
 }
