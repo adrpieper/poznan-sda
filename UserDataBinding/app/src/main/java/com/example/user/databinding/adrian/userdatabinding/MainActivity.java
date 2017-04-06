7cfb0fdacf084ce15eced15a44bb83b927130f34
Adrian <adrpieper@gmail.com>
2017-04-06 19:16:54 +0200

    Dodałem przycisk

--- UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/MainActivity.java
+++ UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.user.databinding.adrian.userdatabinding;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.user.databinding.adrian.userdatabinding.databinding.ActivityMainBinding;
 
@@ -11,8 +12,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setUser(new User("Jan", "Kowalski"));
-
+        binding.nextPersonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.setUser(new User("Adam", "Dominik"));
+            }
+        });
     }
 }
