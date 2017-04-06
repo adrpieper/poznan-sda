dd38de04f17ccd199beebfbec12963bdb43e4393
Adrian <adrpieper@gmail.com>
2017-04-06 20:47:36 +0200

    Utworzyłem projekt Tax Form

---
+++ TaxForm/app/src/main/java/com/example/tax/databinding/adrian/taxform/TaxModel.java
@@ -0,0 +1,41 @@
+package com.example.tax.databinding.adrian.taxform;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+/**
+ * Created by Adrian on 2017-04-06.
+ */
+
+public class TaxModel extends BaseObservable {
+
+    private double amount;
+    private double taxPercentage;
+    private double freeAmount;
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+        notifyPropertyChanged(BR._all);
+    }
+
+    public void setFreeAmount(double freeAmount) {
+        this.freeAmount = freeAmount;
+        notifyPropertyChanged(BR._all);
+    }
+
+    public void setTaxPercentage(double taxPercentage) {
+        this.taxPercentage = taxPercentage;
+        notifyPropertyChanged(BR._all);
+    }
+
+    @Bindable
+    public double getTaxAmount() {
+        return 0;
+    }
+
+    @Bindable
+    public double getNetAmount() {
+        return 0;
+    }
+
+}
