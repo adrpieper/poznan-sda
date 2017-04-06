77a22beca1f9d13cc3a837ca9f9a3456fa46dd0c
Adrian <adrpieper@gmail.com>
2017-04-06 18:12:56 +0200

    Dodałem id do textView

--- UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/MainActivity.java
+++ UserDataBinding/app/src/main/java/com/example/user/databinding/adrian/userdatabinding/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.user.databinding.adrian.userdatabinding;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+
+import com.example.user.databinding.adrian.userdatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.textView.setText("Mój text! - TEST");
+
     }
 }
