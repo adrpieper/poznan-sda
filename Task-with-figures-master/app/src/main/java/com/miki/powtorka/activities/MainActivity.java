60a062f3bf64b36efccda788a9f08484ae0fdee6
Adrian <adrpieper@gmail.com>
2017-04-06 17:56:07 +0200

    Utworzyłem User DataBinding Example

--- Task-with-figures-master/app/src/main/java/com/miki/powtorka/activities/MainActivity.java
+++ Task-with-figures-master/app/src/main/java/com/miki/powtorka/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.miki.powtorka.R;
