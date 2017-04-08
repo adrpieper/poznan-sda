0f02b7f5dedaf0010e9a9c9a56bbe82e0800da9a
Adrian <adrpieper@gmail.com>
2017-04-08 14:46:51 +0200

    Dodałem projekt prezentujący Dependency Injection

---
+++ DaggerExample/app/src/main/java/com/example/dagger/adrian/daggerexample/model/Building.java
@@ -0,0 +1,8 @@
+package com.example.dagger.adrian.daggerexample.model;
+
+/**
+ * Created by Adrian on 2017-04-08.
+ */
+
+public class Building {
+}
---
+++ DaggerExample/app/src/main/java/com/example/dagger/adrian/daggerexample/model/Population.java
@@ -0,0 +1,13 @@
+package com.example.dagger.adrian.daggerexample.model;
+
+/**
+ * Created by Adrian on 2017-04-08.
+ */
+
+public class Population {
+    private final Economy economy;
+
+    public Population(Economy economy) {
+        this.economy = economy;
+    }
+}
