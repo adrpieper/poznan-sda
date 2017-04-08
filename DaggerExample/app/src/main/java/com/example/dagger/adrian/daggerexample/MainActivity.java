88910d99a65fa962bac29d2056a0f22ce6c10a9b
Adrian <adrpieper@gmail.com>
2017-04-08 15:22:41 +0200

    Dodałem interfejs

--- DaggerExample/app/src/main/java/com/example/dagger/adrian/daggerexample/MainActivity.java
+++ DaggerExample/app/src/main/java/com/example/dagger/adrian/daggerexample/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.dagger.adrian.daggerexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.dagger.adrian.daggerexample.model.State;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button calculateButton = (Button) findViewById(R.id.calculateButton);
+        final TextView goldTextView = (TextView) findViewById(R.id.goldTextView);
+
+        final State state = DependencyResolver.resolve();
+        goldTextView.setText(String.valueOf(state.getGold()));
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                state.countBalance();
+                goldTextView.setText(String.valueOf(state.getGold()));
+            }
+        });
     }
 }
